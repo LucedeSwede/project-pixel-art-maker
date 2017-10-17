01328dbeda7395dd7b751717e27a69451508d7e9
lucedeswede <nathanielcarolina@gmail.com>
2017-10-17 21:28:58 +0800

    Update ids to classes

--- designs.js
+++ designs.js
@@ -4,10 +4,10 @@
 // When size is submitted by the user, call makeGrid()
 
 
-$('#input_submit').click(function () {
-    $('#pixel_canvas').append(function makeGrid() {
-        const inputHeight = $('#input_height').val();
-        const inputWidth = $('#input_width').val();
+$('.inputSubmit').click(function () {
+    $('.pixelCanvas').append(function makeGrid() {
+        const inputHeight = $('.inputHeight').val();
+        const inputWidth = $('.inputWidth').val();
         let table = "";
         for (i = 0; i < inputHeight; i++) {
             table += "<tr>";
@@ -18,10 +18,10 @@ $('#input_submit').click(function () {
         }
         return table;
     });
-    $('#input_submit').remove();
-    $('#sizePicker').append('<input type="button" class="startOver" value="Start Over!" onclick="window.location.reload()">');
+    $('.inputSubmit').remove();
+    $('.sizePicker').append('<input type="button" class="startOver" value="Start Over!" onclick="window.location.reload()">');
 });
 
-$('#pixel_canvas').on('click', 'td', function () {
-    $(this).css('background-color', $('#colorPicker').val());
+$('.pixelCanvas').on('click', 'td', function () {
+    $(this).css('background-color', $('.colorPicker').val());
 });
