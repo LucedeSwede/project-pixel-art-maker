0cd55f5878ead827e7d5fb1455a0084da22da46f
lucedeswede <nathanielcarolina@gmail.com>
2017-10-17 21:45:14 +0800

    Add comments to js file

--- designs.js
+++ designs.js
@@ -1,13 +1,10 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-
+// When size is submitted by user, this will append the specified rows and cols to table element:
 $('.inputSubmit').click(function () {
     $('.pixelCanvas').append(function makeGrid() {
+        // Gets the user specified values for grid height and width:
         const INPUT_HEIGHT = $('.inputHeight').val();
         const INPUT_WIDTH = $('.inputWidth').val();
+        // Generates html code for table:
         let table = "";
         for (i = 0; i < INPUT_HEIGHT; i++) {
             table += "<tr>";
@@ -18,10 +15,12 @@ $('.inputSubmit').click(function () {
         }
         return table;
     });
+    // Removes Submit button after click and replaces it with a Start Over! button:
     $('.inputSubmit').remove();
     $('.sizePicker').append('<input type="button" class="startOver" value="Start Over!" onclick="window.location.reload()">');
 });
 
+// Replaces table cell background color with selected color on click
 $('.pixelCanvas').on('click', 'td', function () {
     $(this).css('background-color', $('.colorPicker').val());
 });
